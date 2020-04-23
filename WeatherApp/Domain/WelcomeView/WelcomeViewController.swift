e849d1b04a68e2cd6eb5fa000b17dcfee0bec10e
fbugrademirel <[farukbugrademirel@gmail.com]>
2020-04-23 21:02:55 +0200

    Init Welcome VC

--- WeatherApp/Domain/WelcomeView/WelcomeViewController.swift
+++ WeatherApp/Domain/WelcomeView/WelcomeViewController.swift
@@ -11,55 +11,70 @@ import CoreLocation
 
 class WelcomeViewController: UIViewController {
 
+    //MARK: - Class Properties
     var viewModel: WelcomeViewModel!
-    let weatherRepo = WeatherRepository.shared
-    let cityRepo = CityListRepository.shared
+//    let weatherRepo = WeatherRepository.shared
+//    let cityRepo = CityListRepository.shared
 
     @IBOutlet private var textField: UITextField!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var cityName: UILabel!
 
 
+    //MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         //refreshWeather()
     }
 
-    @IBAction func searchPressed(_ sender: UIButton) {
-        if let text = textField.text {
-            cityRepo.getCityInfo(by: text) { [weak self] (data) in
-                guard let self = self else {return}
-                print(data[0])
-                DispatchQueue.main.async {
-                    self.refreshWeather(by: data[0].id)
-                }
-            }
-        }
-    }
-    
-    @IBAction func refreshPressed(_ sender: UIButton) {
-//        refreshWeather()
-    }
 
-    func refreshWeather(by id: Int) {
-        weatherRepo.getCurrentWeatherInfo(with:.id(id)) { [weak self] (data) in
-            guard let self = self else { return }
-            self.view.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
-            if let data = data {
-                DispatchQueue.main.async {
-                    let formatter = DateFormatter()
-                    formatter.locale = NSLocale.current
-                    formatter.dateFormat = "MMM dd HH:mm"
-                    if let timeInterval = TimeInterval(exactly: Double(data.dt)) {
-                        let date = Date(timeIntervalSince1970: timeInterval)
-                        self.label.text = formatter.string(from: date)
-                        self.cityName.text = data.name
-                    }
-                }
-            }
-        }
-    }
+
+
+
+
+
+
+
+    
+
+
+
+
+//    @IBAction func searchPressed(_ sender: UIButton) {
+//        if let text = textField.text {
+//            cityRepo.getCityInfo(by: text) { [weak self] (data) in
+//                guard let self = self else {return}
+//                print(data[0])
+//                DispatchQueue.main.async {
+//                    self.refreshWeather(by: data[0].id)
+//                }
+//            }
+//        }
+//    }
+//
+//    @IBAction func refreshPressed(_ sender: UIButton) {
+////        refreshWeather()
+//    }
+//
+//    func refreshWeather(by id: Int) {
+//        weatherRepo.getCurrentWeatherInfo(with:.id(id)) { [weak self] (data) in
+//            guard let self = self else { return }
+//            self.view.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
+//            if let data = data {
+//                DispatchQueue.main.async {
+//                    let formatter = DateFormatter()
+//                    formatter.locale = NSLocale.current
+//                    formatter.dateFormat = "MMM dd HH:mm"
+//                    if let timeInterval = TimeInterval(exactly: Double(data.dt)) {
+//                        let date = Date(timeIntervalSince1970: timeInterval)
+//                        self.label.text = formatter.string(from: date)
+//                        self.cityName.text = data.name
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
 
 // MARK: - StoryboardInstantiable
